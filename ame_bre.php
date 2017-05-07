5c0dd7d9e7c7eae159b9c6fbf3db6fe65ed47fc4
radislavsplichal <radislavsplichal@gmail.com>
2017-05-07 21:59:09 +0200

    American English vs British English Release Candidate RC1
    ame_bre.csv file should be proof read and tested out.
    Nice introduction and some words would be usefull.
    The procedure should be documented

--- ame_bre.php
+++ ame_bre.php
@@ -1,19 +1,17 @@
 <html>
 <table class="table resposive">
 
-  <script>
+<script>
 
  function playSound (id){
-   alert(id);
+    //alert(id);
     var audio = document.getElementById(id);
 
-    //console.log(id);
-    // console.log(idSet);
-    audio.setAttribute('src', 'media/soundFiles/ame_bre/'+id);
+    audio.setAttribute('src',"media/soundFiles/ame_bre/"+id);
     //audio.load();
     audio.play();
  }
-  </script>
+</script>
 
 <?php
 
@@ -23,8 +21,9 @@ $row = 1;
 if (($handle = fopen("media/soundFiles/ame_bre.csv", "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
       $num = count($data);
-      //echo '<tr><td><div onClick="playSound("'.$data[1].'")">'.$data[0].'<audio id="'.$data[1].'"></audio></div></td> <td><div onClick="playSound("'.$data[3].'")">'.$data[2].'<audio id="'.$data[3].'"></audio></div></td></tr>';
-      echo '<tr><td><div onClick="playSound("'.$data[1].'")">'.$data[0].'<audio id="'.$data[1].'"></audio></div></td><td><div onClick="playSound("'.$data[3].'")">'.$data[2].'<audio id="'.$data[3].'"></audio></div></td></tr>';
+      $fc1 = '"'.$data[1].'"';
+      $fc2 = '"'.$data[3].'"';
+    echo '<tr><td><div onClick=\'playSound('.$fc1.')\'>'.$data[0].'<audio id="'.$data[1].'"></audio></div></td><td><div onClick=\'playSound('.$fc2.')\'>'.$data[2].'<audio id="'.$data[3].'"></audio></div></td></tr>';
 
 
       $row++;
